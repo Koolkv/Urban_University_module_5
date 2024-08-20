1d1277d8b6c95c7c23d1ad1f14bf8b7ff69a4b2f
Nikita <90nk3666@gmail.com>
2024-08-20 23:21:50 +0300

    Домашняя работа Модуль 5

---
+++ module_5_2.py
@@ -0,0 +1,28 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            print(f'{new_floor} этаж')
+        else:
+            print(
+                f'{new_floor} - в "{self.name}" такого этажа не существует. Выше {self.number_of_floors}-го этажа '
+                f'не подняться.')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
