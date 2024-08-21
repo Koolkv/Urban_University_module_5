dcb32c8329b33544b49d54b214fb07697630c9b8
Nikita <90nk3666@gmail.com>
2024-08-21 16:06:03 +0300

    Домашняя работа Модуль 5

--- module_5_1.py
+++ module_5_1.py
@@ -5,14 +5,20 @@ class House:
 
     def go_to(self, new_floor):
         if 1 <= new_floor <= self.number_of_floors:
-            print(f'{new_floor} этаж')
+            for i in range(1, new_floor + 1):
+                print(f'{i} этаж')
         else:
-            print(
-                f'{new_floor} - в "{self.name}" такого этажа не существует. Выше {self.number_of_floors}-го этажа '
-                f'не подняться.')
+            if new_floor <= 0:
+                print(
+                    f'{new_floor} - в "{self.name}" такого этажа не существует. Ниже 1-го этажа '
+                    f'только с лопатой.')
+            else:
+                print(
+                    f'{new_floor} - в "{self.name}" такого этажа не существует. Выше {self.number_of_floors}-го этажа '
+                    f'не подняться.')
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(5)
+h1.go_to(-5)
 h2.go_to(10)
