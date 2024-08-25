5f82d51272c43eb476bb5f1684732aca04bf688c
Nikita <90nk3666@gmail.com>
2024-08-25 22:31:54 +0300

    Домашняя работа Модуль 5

--- module_5_3.py
+++ module_5_3.py
@@ -18,11 +18,8 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
-
-    # def __eq__(self, other):
-    #     if isinstance(other, int):    # не работает проверка на int
-    #         return self.number_of_floors == other.number_of_floors #
+        if isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
