19b5f762b16b4eb1511a41aacb2303fbf67cf767
Nikita <90nk3666@gmail.com>
2024-08-27 23:53:53 +0300

    Домашняя работа Модуль 5

--- module_5_3.py
+++ module_5_3.py
@@ -18,32 +18,41 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        if isinstance(other, int):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
             return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        return self.number_of_floors + value
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        return self
 
-    def __radd__(self, value):
-        return self.number_of_floors + value
+    def __radd__(self, other):
+        return self.__add__(other)
 
-    def __iadd__(self, value):
-        return self.number_of_floors + value
+    def __iadd__(self, other: int):
+        return self.__add__(other)
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -69,3 +78,76 @@ print(h1 >= h2)  # __ge__
 print(h1 < h2)  # __lt__
 print(h1 <= h2)  # __le__
 print(h1 != h2)  # __ne__
+
+#
+# class House:
+#     def __init__(self, name, number_of_floors: int):
+#         self.name = name
+#         self.number_of_floors = number_of_floors
+#
+#     def go_to(self, new_floor: int):
+#         if 1 <= new_floor <= self.number_of_floors:
+#             print(f'{new_floor} этаж')
+#         else:
+#             print(
+#                 f'{new_floor} - в "{self.name}" такого этажа не существует. Выше {self.number_of_floors}-го этажа '
+#                 f'не подняться.')
+#
+#     def __len__(self):
+#         return self.number_of_floors
+#
+#     def __str__(self):
+#         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+#
+#     def __eq__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors == other
+#
+#     def __lt__(self, other):
+#         return self.number_of_floors < other.number_of_floors
+#
+#     def __le__(self, other):
+#         return self.number_of_floors <= other.number_of_floors
+#
+#     def __gt__(self, other):
+#         return self.number_of_floors > other.number_of_floors
+#
+#     def __ge__(self, other):
+#         return self.number_of_floors >= other.number_of_floors
+#
+#     def __ne__(self, other):
+#         return self.number_of_floors != other.number_of_floors
+#
+#     def __add__(self, value):
+#         return self.number_of_floors + value
+#
+#     def __radd__(self, value):
+#         return self.number_of_floors + value
+#
+#     def __iadd__(self, value):
+#         return self.number_of_floors + value
+#
+#
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(h1 == h2)  # __eq__
+#
+# h1 = h1 + 10  # __add__
+# print(h1)
+# print(h1 == h2)
+# #
+# h1 += 10  # __iadd__
+# print(h1)
+# #
+# h2 = 10 + h2  # __radd__
+# print(h2)
+# #
+# print(h1 > h2)  # __gt__
+# print(h1 >= h2)  # __ge__
+# print(h1 < h2)  # __lt__
+# print(h1 <= h2)  # __le__
+# print(h1 != h2)  # __ne__
